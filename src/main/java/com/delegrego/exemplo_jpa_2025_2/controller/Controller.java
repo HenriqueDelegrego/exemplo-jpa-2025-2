81c5a7830296cfc52499d37e399934712a2103b5
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2026-06-30 15:16:55 -0300

    Adiciona menu console

--- src/main/java/com/delegrego/exemplo_jpa_2025_2/controller/Controller.java
+++ src/main/java/com/delegrego/exemplo_jpa_2025_2/controller/Controller.java
@@ -1,5 +1,8 @@
 package com.delegrego.exemplo_jpa_2025_2.controller;
 
+import java.util.List;
+import java.util.Scanner;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -8,8 +11,208 @@ public class Controller implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		System.out.println("Hello, spring");
+
+		Scanner input = new Scanner(System.in);
+		int opcao;
+
+		System.out.print("\n=== Exemplo JPA 1 ===\n");
+
+		do {
+
+			ConsoleUtils.exibirMenu();
+
+			opcao = input.nextInt();
+
+			try {
+				switch (opcao) {
+				case 1 -> cadastrarDepartamento();
+				case 2 -> listarDepartamentos();
+				case 3 -> atualizarDepartamento();
+				case 4 -> deletarDepartamento();
+				case 5 -> cadastrarFuncionario();
+				case 6 -> listarFuncionarios();
+				case 7 -> atualizarFuncionario();
+				case 8 -> deletarFuncionario();
+				case 0 -> System.out.println("Saindo da aplicação...");
+				default -> {
+					System.out.println("Opção inválida\n");
+					ConsoleUtils.exibirMenu();
+					opcao = input.nextInt();
+				}
+				}
+
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+
+		} while (opcao != 0);
+
+	}
+
+	private void cadastrarDepartamento() {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Insira o nome do Departamento: ");
+		String nomeDepartamento = input.nextLine();
+
+		Departamento departamento = new Departamento();
+		departamento.setNomeDepartamento(nomeDepartamento);
+
+		departamentoService.cadastrarDepartamento(departamento);
+
+		System.out.println("Departamento criado!");
+
+	}
+
+	private void listarDepartamentos() {
+
+		List<Departamento> listaDepartamentos = departamentoService.listarDepartamentos();
+
+		if (listaDepartamentos.isEmpty()) {
+			System.out.println("Não há departamentos");
+		} else {
+			System.out.println("Lista de departamentos: ");
+			for (Departamento d : listaDepartamentos) {
+				System.out.println(d);
+			}
+		}
+
+	}
+
+	private void atualizarDepartamento() {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Insira a id do Departamento: ");
+		int id = input.nextInt();
+
+		System.out.println("Insira o nome do Departamento: ");
+		input.nextLine();
+		String nomeDepartamento = input.nextLine();
+
+		Departamento departamento = new Departamento();
+		departamento.setIdDepartamento(id);
+		departamento.setNomeDepartamento(nomeDepartamento);
+
+		departamentoService.atualizarDepartamento(departamento);
+
+		System.out.println("Departamento atualizado!");
+
+	}
+
+	private void deletarDepartamento() {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Insira a id do Departamento: ");
+		int id = input.nextInt();
+
+		departamentoService.deletarDepartamento(id);
+
+		System.out.println("Departamento deletado!");
+
+	}
+
+	private void cadastrarFuncionario() {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Informe o nome: ");
+		String nome = input.nextLine();
+
+		System.out.print("Informe o email: ");
+		String email = input.nextLine();
+
+		System.out.print("Informe a senha: ");
+		String senha = input.nextLine();
+
+		System.out.print("Informe o salário: ");
+		double salario = input.nextDouble();
+
+		System.out.print("Informe o ID do departamento: ");
+		int idDepartamento = input.nextInt();
+
+		Departamento departamento = departamentoService.obterDepartamentoPorId(idDepartamento);
+
+		Funcionario funcionario = new Funcionario();
+		funcionario.setNome(nome);
+		funcionario.setEmail(email);
+		funcionario.setSenha(senha);
+		funcionario.setSalario(salario);
+		funcionario.setDepartamento(departamento);
+
+		funcionarioService.cadastrarFuncionario(funcionario);
+
+		System.out.println("Funcionário criado!");
+
+	}
+
+	private void listarFuncionarios() {
+
+		List<Funcionario> listaFuncionarios = funcionarioService.listarFuncionarios();
+
+		if (listaFuncionarios.isEmpty()) {
+			System.out.println("Não há funcionários");
+		} else {
+			System.out.println("Lista de funcionarios: ");
+			for (Funcionario f : listaFuncionarios) {
+				System.out.println(f);
+			}
+		}
+
+	}
+
+	private void atualizarFuncionario() {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Insira a id do funcionário: ");
+		int id = input.nextInt();
+		input.nextLine();
+
+		System.out.print("Informe o nome: ");
+		String nome = input.nextLine();
+
+		System.out.print("Informe o email: ");
+		String email = input.nextLine();
+
+		System.out.print("Informe a senha: ");
+		String senha = input.nextLine();
+
+		System.out.print("Informe o salário: ");
+		double salario = input.nextDouble();
+
+		System.out.print("Informe o ID do departamento: ");
+		int idDepartamento = input.nextInt();
+
+		Departamento departamento = departamentoService.obterDepartamentoPorId(idDepartamento);
+
+		Funcionario funcionario = new Funcionario();
+		funcionario.setIdFuncionario(id);
+		funcionario.setNome(nome);
+		funcionario.setEmail(email);
+		funcionario.setSenha(senha);
+		funcionario.setSalario(salario);
+		funcionario.setDepartamento(departamento);
+
+		funcionarioService.atualizarFuncionario(funcionario);
+
+		System.out.println("Funcionário atualizado!");
+
+	}
+
+	private void deletarFuncionario() {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Insira a id do Funcionario: ");
+		int id = input.nextInt();
+
+		funcionarioService.deletarFuncionario(id);
+
+		System.out.println("Funcionário deletado!");
+
 	}
 
 }
---
+++ src/main/java/com/delegrego/exemplo_jpa_2025_2/utils/ConsoleUtils.java
@@ -0,0 +1,17 @@
+package com.delegrego.exemplo_jpa_2025_2.utils;
+
+public class ConsoleUtils {
+
+	public static void exibirMenu() {
+		System.out.print("\n1) Criar Departamento\n");
+		System.out.print("2) Listar Departamentos\n");
+		System.out.print("3) Atualizar Departamento\n");
+		System.out.print("4) Deletar Departamento\n");
+		System.out.print("5) Criar Funcionário\n");
+		System.out.print("6) Listar Funcionários\n");
+		System.out.print("7) Atualizar Funcionário\n");
+		System.out.print("8) Deletar Funcionário\n");
+		System.out.print("0) Sair\n");
+	}
+
+}
